0aabac58b2e644f1a8a5434455baa77c234c50ed
PhumPitch <PhumPitchGithub@gmail.com>
2022-12-23 01:48:49 +0700

    WorkShop7

--- OOP_Program/src/CheckStockProduct.java
+++ OOP_Program/src/CheckStockProduct.java
@@ -6,6 +6,7 @@ public class CheckStockProduct {
 	public static void main(String[] args) {
 		System.out.print("How many product list in stock : ");
 		int stockList = scan.nextInt();
+		System.out.println();
 		Product[] checkStock = new Product[stockList];
 		for (int i = 0; i < checkStock.length; i++) {
 
@@ -16,7 +17,29 @@ public class CheckStockProduct {
 			checkStock[i].setUnit(scan.nextInt());
 			System.out.println();
 
-		}//end for
+		} // end for
 		System.out.println("-------------------------------");
+		System.out.println("List of product in \'LOW\' status.");
+		System.out.println("-------------------------------");
+		for (Product chkStock : checkStock) {
+			if (chkStock.getUnit() < 5)
+				System.out.println(">> "+chkStock.getId() + " has " + chkStock.getUnit() + " units");
+		}
+		System.out.println("-------------------------------");
+		System.out.println("List of product in \'NORMAL\' status.");
+		System.out.println("-------------------------------");
+		for (Product chkStock : checkStock) {
+			if (chkStock.getUnit() >= 5 && chkStock.getUnit() <= 50 )
+				System.out.println(">> "+chkStock.getId() + " has " + chkStock.getUnit() + " units");
+		}
+		System.out.println("-------------------------------");
+		System.out.println("List of product in \'HIGH\' status.");
+		System.out.println("-------------------------------");
+		for (Product chkStock : checkStock) {
+			if (chkStock.getUnit() > 50 )
+				System.out.println(">> "+chkStock.getId() + " has " + chkStock.getUnit() + " units");
+		}
+
+
 	}
 }
